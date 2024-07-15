b721477d93f2e0f2c76f694a3a572ef83cf8374a
saikiranmerugu74 <147452666+saikiranmerugu74@users.noreply.github.com>
2024-07-14 20:48:32 -0400

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -2,5 +2,6 @@
 
 set -e
 cd /home/ubuntu
+ansible-playbook -i inventory.ini vm-create.yml --vault-password-file vault.pass
 ansible-playbook -i inventory.ini installapache2.yml --vault-password-file vault.pass
 echo "Running ansible command through script"
