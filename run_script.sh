7bcefd4ed6d28bafc5a93f8ad425a9c539df7c70
saikiranmerugu74 <147452666+saikiranmerugu74@users.noreply.github.com>
2024-07-14 21:00:12 -0400

    Update run_script.sh for test

--- run_script.sh
+++ run_script.sh
@@ -2,6 +2,7 @@
 
 set -e
 pwd
+ls
 #cd /home/ubuntu
 #ansible-playbook -i inventory.ini vm-create.yml --vault-password-file vault.pass
 #ansible-playbook -i inventory.ini installapache2.yml --vault-password-file vault.pass
